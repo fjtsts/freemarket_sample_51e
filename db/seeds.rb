8dbe89b9016c678128e38c28643010a88d4163d2
ShunsukeSugie <psku3ixdae@gmail.com>
2019-05-30 01:29:58 +0900

    commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,55 +8,887 @@ Item.create!(
   day: 1,
   price: 1000
 )
-Category.create!(
-  name: '',
-  parent_id: ""
-)
-Category.create!(
-  name: 'レディース',
-  parent_id: ""
-)
-Category.create!(
-  name: 'メンズ',
-  parent_id: ""
-)
-Category.create!(
-  name: 'ベビー・キッズ',
-  parent_id: ""
-)
-Category.create!(
-  name: 'インテリア・住まい・小物',
-  parent_id: ""
-)
-Category.create!(
-  name: '本・音楽・ゲーム',
-  parent_id: ""
-)
-Category.create!(
-  name: 'おもちゃ・ホビー・グッズ',
-  parent_id: ""
-)
-Category.create!(
-  name: 'コスメ・香水・美容',
-  parent_id: ""
-)
-Category.create!(
-  name: '家電・スマホ・カメラ',
-  parent_id: ""
-)
-Category.create!(
-  name: 'ハンドメイド',
-  parent_id: ""
-)
-Category.create!(
-  name: 'チケット',
-  parent_id: ""
-)
-Category.create!(
-  name: '自動車・オートバイ',
-  parent_id: ""
-)
-Category.create!(
-  name: 'その他',
-  parent_id: ""
-)
+
+# Category.create!(
+#   name: 'レディース',
+#   parent_id: ""
+# )
+# Category.create!(
+#   name: 'メンズ',
+#   parent_id: ""
+# )
+# Category.create!(
+#   name: 'ベビー・キッズ',
+#   parent_id: ""
+# )
+# Category.create!(
+#   name: 'インテリア・住まい・小物',
+#   parent_id: ""
+# )
+# Category.create!(
+#   name: '本・音楽・ゲーム',
+#   parent_id: ""
+# )
+# Category.create!(
+#   name: 'おもちゃ・ホビー・グッズ',
+#   parent_id: ""
+# )
+# Category.create!(
+#   name: 'コスメ・香水・美容',
+#   parent_id: ""
+# )
+
+# Category.create!(
+#   name: '家電・スマホ・カメラ',
+#   parent_id: ""
+# )
+# Category.create!(
+#   name: '',
+#   parent_id: ""
+# )
+# Category.create!(
+#   name: 'ハンドメイド',
+#   parent_id: ""
+# )
+# Category.create!(
+#   name: 'チケット',
+#   parent_id: ""
+# )
+# Category.create!(
+#   name: '自動車・オートバイ',
+#   parent_id: ""
+# )
+# Category.create!(
+#   name: 'その他',
+#   parent_id: ""
+# )
+# Category.create!(
+#   name: 'トップス',
+#   parent_id: 1
+# )
+# Category.create!(
+#   name: 'ジャケット/アウター',
+#   parent_id: 1
+# )
+# Category.create!(
+#   name: 'パンツ',
+#   parent_id: 1
+# )
+# Category.create!(
+#   name: 'スカート',
+#   parent_id: 1
+# )
+# Category.create!(
+#   name: 'ワンピース',
+#   parent_id: 1
+# )
+# Category.create!(
+#   name: '靴',
+#   parent_id: 1
+# )
+# Category.create!(
+#   name: 'ルームウェア/パジャマ',
+#   parent_id: 1
+# )
+# Category.create!(
+#   name: 'レッグウェア',
+#   parent_id: 1
+# )
+# Category.create!(
+#   name: '帽子',
+#   parent_id: 1
+# )
+# Category.create!(
+#   name: 'バッグ',
+#   parent_id: 1
+# )
+# Category.create!(
+#   name: 'アクセサリー',
+#   parent_id: 1
+# )
+# Category.create!(
+#   name: 'ヘアアクセサリー',
+#   parent_id: 1
+# )
+# Category.create!(
+#   name: '小物',
+#   parent_id: 1
+# )
+# Category.create!(
+#   name: '時計',
+#   parent_id: 1
+# )
+# Category.create!(
+#   name: 'ウィッグ/エクステ',
+#   parent_id: 1
+# )
+# Category.create!(
+#   name: '浴衣/..水着',
+#   parent_id: 1
+# )
+# Category.create!(
+#   name: 'スーツ/フォーマル/ドレス',
+#   parent_id: 1
+# )
+# Category.create!(
+#   name: 'マタニティー',
+#   parent_id: 1
+# )
+# Category.create!(
+#   name: 'その他',
+#   parent_id: 1
+# )
+# Category.create!(
+#   name: 'トップス',
+#   parent_id: 2
+# )
+# Category.create!(
+#   name: 'ジャケット/アウター',
+#   parent_id: 2
+# )
+# Category.create!(
+#   name: 'パンツ',
+#   parent_id: 2
+# )
+# Category.create!(
+#   name: '靴',
+#   parent_id: 2
+# )
+# Category.create!(
+#   name: 'バッグ',
+#   parent_id: 2
+# )
+# Category.create!(
+#   name: 'スーツ',
+#   parent_id: 2
+# )
+# Category.create!(
+#   name: '帽子',
+#   parent_id: 2
+# )
+# Category.create!(
+#   name: 'アクセサリー',
+#   parent_id: 2
+# )
+# Category.create!(
+#   name: '小物',
+#   parent_id: 2
+# )
+# Category.create!(
+#   name: '時計',
+#   parent_id: 2
+# )
+# Category.create!(
+#   name: '水着',
+#   parent_id: 2
+# )
+# Category.create!(
+#   name: 'レッグウェア',
+#   parent_id: 2
+# )
+# Category.create!(
+#   name: 'アンダーウェア',
+#   parent_id: 2
+# )
+# Category.create!(
+#   name: 'その他',
+#   parent_id: 2
+# )
+# Category.create!(
+#   name: 'ベビー服（女の子用）~95cm',
+#   parent_id: 3
+# )
+# Category.create!(
+#   name: 'ベビー服（男の子用）~95cm',
+#   parent_id: 3
+# )
+# Category.create!(
+#   name: 'ベビー服（男女兼用）~95cm',
+#   parent_id: 3
+# )
+# Category.create!(
+#   name: 'ベビー服（女の子用）~100cm',
+#   parent_id: 3
+# )
+# Category.create!(
+#   name: 'ベビー服(男の子用）~100cm',
+#   parent_id: 3
+# )
+# Category.create!(
+#   name: 'ベビー服(男女兼用）~100cm',
+#   parent_id: 3
+# )
+# Category.create!(
+#   name: 'キッズ靴',
+#   parent_id: 3
+# )
+# Category.create!(
+#   name: '子供用ファッション小物',
+#   parent_id: 3
+# )
+# Category.create!(
+#   name: 'おむつ/トイレ/バス',
+#   parent_id: 3
+# )
+# Category.create!(
+#   name: '外出/移動用品',
+#   parent_id: 3
+# )
+# Category.create!(
+#   name: '授乳/ 食事/室内用品',
+#   parent_id: 3
+# )
+# Category.create!(
+#   name: 'ベビー家具/寝具/室内用品',
+#   parent_id: 3
+# )
+# Category.create!(
+#   name: 'おもちゃ',
+#   parent_id: 3
+# )
+# Category.create!(
+#   name: '行事/記念品',
+#   parent_id: 3
+# )
+# Category.create!(
+#   name: 'その他',
+#   parent_id: 3
+# )
+# Category.create!(
+#   name: 'キッチン/食器',
+#   parent_id: 4
+# )
+# Category.create!(
+#   name: 'ベッド/ソファベッド',
+#   parent_id: 4
+# )
+# Category.create!(
+#   name: '椅子/チェアー',
+#   parent_id: 4
+# )
+# Category.create!(
+#   name: '机/テーブル',
+#   parent_id: 4
+# )
+# Category.create!(
+#   name: '収納家具',
+#   parent_id: 4
+# )
+# Category.create!(
+#   name: 'ラグ／カーペット/マット',
+#   parent_id: 4
+# )
+# Category.create!(
+#   name: 'カーテン/ブラインド',
+#   parent_id: 4
+# )
+# Category.create!(
+#   name: 'ライト/証明',
+#   parent_id: 4
+# )
+# Category.create!(
+#   name: '寝具',
+#   parent_id: 4
+# )
+# Category.create!(
+#   name: 'インテリア小物',
+#   parent_id: 4
+# )
+# Category.create!(
+#   name: '季節/年中行事',
+#   parent_id: 4
+# )
+# Category.create!(
+#   name: 'その他',
+#   parent_id: 4
+# )
+# Category.create!(
+#   name: '本',
+#   parent_id: 5
+# )
+# Category.create!(
+#   name: '漫画',
+#   parent_id: 5
+# )
+# Category.create!(
+#   name: '雑誌',
+#   parent_id: 5
+# )
+# Category.create!(
+#   name: 'CD',
+#   parent_id: 5
+# )
+# Category.create!(
+#   name: 'DVD/ブルーレイ',
+#   parent_id: 5
+# )
+# Category.create!(
+#   name: 'レコード',
+#   parent_id: 5
+# )
+# Category.create!(
+#   name: 'テレビゲーム',
+#   parent_id: 5
+# )
+# Category.create!(
+#   name: 'おもちゃ',
+#   parent_id: 6
+# )
+# Category.create!(
+#   name: 'タレントグッズ',
+#   parent_id: 6
+# )
+# Category.create!(
+#   name: 'コミック/アニメグッズ',
+#   parent_id: 6
+# )
+# Category.create!(
+#   name: 'トレーディングカード',
+#   parent_id: 6
+# )
+# Category.create!(
+#   name: 'フィギュア',
+#   parent_id: 6
+# )
+# Category.create!(
+#   name: '楽器/器材',
+#   parent_id: 6
+# )
+# Category.create!(
+#   name: 'コレクション',
+#   parent_id: 6
+# )
+# Category.create!(
+#   name: 'ミリタリー',
+#   parent_id: 6
+# )
+# Category.create!(
+#   name: '美術品',
+#   parent_id: 6
+# )
+# Category.create!(
+#   name: 'アート用品',
+#   parent_id: 6
+# )
+# Category.create!(
+#   name: 'その他',
+#   parent_id: 6
+# )
+# Category.create!(
+#   name: 'ベースメイク',
+#   parent_id: 7
+# )
+# Category.create!(
+#   name: 'メイクアップ',
+#   parent_id: 7
+# )
+# Category.create!(
+#   name: 'ネイルケア',
+#   parent_id: 7
+# )
+#   Category.create!(
+#     name: '香水',
+#     parent_id: 7
+# )
+# Category.create!(
+#   name: 'スキンケア/基礎化粧品',
+#   parent_id: 7
+# )
+# Category.create!(
+#   name: 'ヘアケア',
+#   parent_id: 7
+# )
+# Category.create!(
+#   name: 'ボディケア',
+#   parent_id: 7
+# )
+# Category.create!(
+#   name: 'オーラルケア',
+#   parent_id: 7
+# )
+# Category.create!(
+#   name: 'リラクゼーション',
+#   parent_id: 7
+# )
+# Category.create!(
+#   name: 'ダイエット',
+#   parent_id: 7
+# )
+# Category.create!(
+#   name: 'その他',
+#   parent_id: 7
+# )
+# Category.create!(
+#   name: 'スマートフォン/携帯電話',
+#   parent_id: 8
+# )
+# Category.create!(
+#   name: 'スマホアクセサリー',
+#   parent_id: 8
+# )
+# Category.create!(
+#   name: 'pc/タブレット',
+#   parent_id: 8
+# )
+# Category.create!(
+#   name: 'カメラ',
+#   parent_id: 8
+# )
+# Category.create!(
+#   name: 'テレビ/映像機器',
+#   parent_id: 8
+# )
+# Category.create!(
+#   name: 'オーディオ機器',
+#   parent_id: 8
+# )
+# Category.create!(
+#   name: '美容/健康',
+#   parent_id: 8
+# )
+# Category.create!(
+#   name: '冷暖房/空調',
+#   parent_id: 8
+# )
+# Category.create!(
+#   name: '生活家電',
+#   parent_id: 8
+# )
+# Category.create!(
+#   name: 'その他',
+#   parent_id: 8
+# )
+# Category.create!(
+#   name: 'ゴルフ',
+#   parent_id: 9
+# )
+# Category.create!(
+#   name: 'フィッシング',
+#   parent_id: 9
+# )
+# Category.create!(
+#   name: '自転車',
+#   parent_id: 9
+# )
+# Category.create!(
+#   name: 'トレーニング/エクササイズ',
+#   parent_id: 9
+# )
+# Category.create!(
+#   name: '野球',
+#   parent_id: 9
+# )
+# Category.create!(
+#   name: 'サッカー/フットサル',
+#   parent_id: 9
+# )
+# Category.create!(
+#   name: 'テニス',
+#   parent_id: 9
+# )
+# Category.create!(
+#   name: 'スノーボード',
+#   parent_id: 9
+# )
+# Category.create!(
+#   name: 'スキー',
+#   parent_id: 9
+# )
+# Category.create!(
+#   name: 'その他のスポーツ',
+#   parent_id: 9
+# )
+# Category.create!(
+#   name: 'アウトドア',
+#   parent_id: 9
+# )
+# Category.create!(
+#   name: 'その他',
+#   parent_id: 9
+# )
+# Category.create!(
+#   name: 'アクセサリー(女性用)',
+#   parent_id: 10
+# )
+# Category.create!(
+#   name: 'ファッション/小物',
+#   parent_id: 10
+# )
+# Category.create!(
+#   name: 'アクセサリー/時計',
+#   parent_id: 10
+# )
+# Category.create!(
+#   name: '日用品/インテリア',
+#   parent_id: 10
+# )
+# Category.create!(
+#   name: '趣味/おもちゃ',
+#   parent_id: 10
+# )
+# Category.create!(
+#   name: 'キッズ/ベビー',
+#   parent_id: 10
+# )
+# Category.create!(
+#   name: '素材/材料',
+#   parent_id: 10
+# )
+# Category.create!(
+#   name: '二次創作',
+#   parent_id: 10
+# )
+# Category.create!(
+#   name: 'その他',
+#   parent_id: 10
+# )
+# Category.create!(
+#   name: '音楽',
+#   parent_id: 11
+# )
+# Category.create!(
+#   name: 'スポーツ',
+#   parent_id: 11
+# )
+# Category.create!(
+#   name: '演劇/芸能',
+#   parent_id: 11
+# )
+# Category.create!(
+#   name: 'イベント',
+#   parent_id: 11
+# )
+# Category.create!(
+#   name: '映画',
+#   parent_id: 11
+# )
+# Category.create!(
+#   name: '施設利用券',
+#   parent_id: 11
+# )
+# Category.create!(
+#   name: '優待券',
+#   parent_id: 11
+# )
+# Category.create!(
+#   name: 'その他',
+#   parent_id: 11
+# )
+# Category.create!(
+#   name: '自動車本体',
+#   parent_id: 12
+# )
+# Category.create!(
+#   name: '自動車タイヤ/ホイール',
+#   parent_id: 12
+# )
+# Category.create!(
+#   name: '自動車パーツ',
+#   parent_id: 12
+# )
+# Category.create!(
+#   name: '自動車アクセサリー',
+#   parent_id: 12
+# )
+# Category.create!(
+#   name: 'オートバイ本体',
+#   parent_id: 12
+# )
+# Category.create!(
+#   name: 'オートバイパーツ',
+#   parent_id: 12
+# )
+# Category.create!(
+#   name: 'オートバイアクセアリー',
+#   parent_id: 12
+# )
+# Category.create!(
+#   name: 'まとめ売り',
+#   parent_id: 13
+# )
+# Category.create!(
+#   name: 'ペット用品',
+#   parent_id: 13
+# )
+# Category.create!(
+#   name: '食品',
+#   parent_id: 13
+# )
+# Category.create!(
+#   name: '飲料/酒',
+#   parent_id: 13
+# )
+# Category.create!(
+#   name: '日用品/生活雑貨/旅行',
+#   parent_id: 13
+# )
+# Category.create!(
+#   name: 'アンティーク/コレクション',
+#   parent_id: 13
+# )
+# Category.create!(
+#   name: '文房具/事務用品',
+#   parent_id: 13
+# )
+# Category.create!(
+#   name: '事務/店舗用品',
+#   parent_id: 13
+# )
+# Category.create!(
+#   name: 'その他',
+#   parent_id: 13
+# )
+
+  
+# %w{テント 寝袋}.each_with_index do |name|
+#   Category.create!(name: name,parent_id: index)
+# end
+  
+
+    categories = %w{
+    Tシャツ/カットソー(半袖/袖なし)
+    Tシャツ/カットソー（七分/長袖)
+    シャツ/ブラウス(半袖/袖なし)
+    シャツ/ブラウス(七分/長袖)
+    ポロシャツ
+    キャミソール
+    タンクトップ
+    ホルターネック
+    ニット/セーター
+    チュニック
+    カーディガン/ボレロ
+    アンサンブル
+    ベスト/ジレ
+    パーカー
+    トレーナー/スウェト
+    ペアトップ/チューブトップ
+    ジャージ
+    その他
+    num = 1
+    }
+    # categories_15 = %w{
+    #   テーラードジャケット
+    #   ノーカラージャケット
+    #   Gジャン/デニムジャケット
+    #   レザージャケット
+    #   ダウンジャケット
+    #   ライダースジャケット
+    #   ミリタリージャケット
+    #   ダウンベスト
+    #   ジャンパー/ブルゾン
+    #   ポンチョ
+    #   ロングコート
+    #   トレンチコート
+    #   ダッフルコート
+    #   ピーコート
+    #   チェスターコート
+    #   モッズコート
+    #   スタジャン
+    #   毛皮/ファーコート
+    #   スプリングコート
+    #   スカジャン
+    #   その他
+    #   num +=1
+    # }
+    # categories_16 = %w{
+    #   デニム/ジーンズ
+    #   ショートパンツ
+    #   カジュアルパンツ
+    #   ハーフパンツ
+    #   チノパン
+    #   ワークパンツ/カーゴパンツ
+    #   クロップドパンツ
+    #   サロペット/オーバーオール
+    #   オールインワン
+    #   サルエルパンツ
+    #   ガウチョパンツ
+    #   その他
+    #   num += 1
+    # }
+    # categories_17 = %w{
+    #   ミニスカート
+    #   ひざ丈スカート
+    #   ロングスカート
+    #   キュロット
+    #   その他
+    #   num += 1
+    # }
+    # categories_18 = %w{
+    #   ミニワンピース
+    #   ひざ丈ワンピース
+    #   ロングワンピース
+    #   その他
+    #   num += 1
+    # }
+    # categories_19 = %w{
+    #   ハイヒール/パンプス
+    #   ブーツ
+    #   サンダル
+    #   スニーカー
+    #   ミュール
+    #   モカシン
+    #   ローファー/革靴
+    #   フラットシューズ/バレーシューズ
+    #   長靴/レインシューズ
+    #   その他
+    #   num += 1
+    # }
+    # categories_20 = %w{
+    #   パジャマ
+    #   ルームウェア
+    #   num += 1
+    # }
+    # categories_21 = %w{
+    #   ソックス
+    #   スパッツ/レギンス
+    #   ストッキング/タイツ
+    #   レグウォーマー
+    #   その他
+    #   num += 1
+    # }
+    # categories_22 = %w{
+    #  ニットキャップ/ビーニー
+    #  ハット
+    #  ハンチング/ベレー帽
+    #  キャップ
+    #  キャスケット
+    #  麦わら帽子
+    #  その他
+    #  num += 1
+    # }
+    # categories_23 = %w{
+    #   ハンドバッグ
+    #   トートバッグ
+    #   エコバッグ
+    #   リュック/バックパック
+    #   ボストンバッグ
+    #   スポーツバッグ
+    #   ショルダーバッグ
+    #   クラッチバッグ
+    #   ポーチ/バニティ
+    #   バディーバッグ/ウェストバッグ
+    #   マザーバッグ
+    #   メッセンジャーバッグ
+    #   ビジネスバッグ
+    #   旅行用カバン/キャリーバッグ
+    #   ショップ袋
+    #   和装用バッグ
+    #   かごバッグ
+    #   その他
+    #   num += 1
+    # }
+    # categories_24 = %w{
+    #   ネックレス
+    #   ブレスレット
+    #   バングル/リストバンド
+    #   リング
+    #   ピアス(片耳用)
+    #   ピアス(両耳用)
+    #   イヤリング
+    #   アンクレット
+    #   ブローチ/コサージュ
+    #   チャーム
+    #   その他
+    # }
+    # categories_25 = %w{
+    #   ヘアゴム
+    #   ヘアバンド/カチューシャ
+    #   ヘアピン
+    #   その他
+    # }
+    # categories_26 = %w{
+    #   長財布
+    #   折り財布
+    #   コインケース/小銭入れ
+    #   名刺入れ/定期入れ
+    #   キーケース
+    #   キーホルーダー
+    #   手袋/アームカバー
+    #   ハンカチ
+    #   ベルト
+    #   マフラー/ショール
+    #   ストール/スヌード
+    #   バンダナ/スカーフ
+    #   ネックウォーマー
+    #   サスペンダー
+    #   サングラス/メガネ
+    #   モバイルケース/カバー
+    #   手袋
+    #   イヤマフラー
+    #   傘
+    #   レインコート/ポンチョ
+    #   ミラー
+    #   タバコグッズ
+    #   その他
+    # }
+    # categories_27 = %w{
+    #   腕時計(アナログ)
+    #   腕時計(デジタル)
+    #   ラバーベルト
+    #   レザーベルト
+    #   金属ベルト
+    #   その他
+    # }
+    # categories_28 = %w{
+    #   前髪ウイッグ
+    #   ロングストレート
+    #   ロングカール
+    #   ショートストレート
+    #   ショートカール
+    #   その他
+    # }
+    # categories_29 = %w{
+    #   浴衣
+    #   着物
+    #   振袖
+    #   長襦袢/半襦袢
+    #   水着セパレート
+    #   水着ワンピース
+    #   水着スポーツ用
+    #   その他
+    # }
+    # categories_30 = %w{
+    #   スカートスーツ上下
+    #   パンツスーツ上下
+    #   ドレス
+    #   パーティーバッグ
+    #   シューズ
+    #   ウェディング
+    #   その他
+    # }
+    # categories_31 = %w{
+    #   トップス
+    #   アウター
+    #   インナー
+    #   ワンピース
+    #   パンツ/スパッツ
+    #   スカート
+    #   パジャマ
+    #   授乳服
+    #   その他
+    # }
+    # categories_32 = %w{
+    #   コスプレ
+    #   下着
+    #   その他
+    # }
+
+
+
+ 
+  
+  categories.each do |category|
+    Category.create!(name: category,parent_id: num)
+    num+= 1
+
+end
+
+
+
+
+
+
+
+
+
+
