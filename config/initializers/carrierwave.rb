3f9ca58adffb0b9f65284c06e35847edbac28478
satoshi fujita <xxxx3104@yahoo.co.jp>
2019-05-29 11:00:03 +0900

    [WIP]UpdateCarrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,15 +2,23 @@ require 'carrierwave/storage/abstract'
 require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
-CarrierWave.configure do |config|
-  config.storage = :fog
-  config.fog_provider = 'fog/aws'
-  config.fog_credentials = {
-    provider: 'AWS',
-    aws_access_key_id: Rails.application.secrets.aws_access_key_id,
-    aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,
-    region: 'ap-northeast-1'
-  }
+# CarrierWave.configure do |config|
+#   config.storage = :fog
+#   config.fog_provider = 'fog/aws'
+#   config.fog_credentials = {
+#     provider: 'AWS',
+#     aws_access_key_id: Rails.application.secrets.aws_access_key_id,
+#     aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,
+#     region: 'ap-northeast-1'
+#   }
+
+# Default value for default_env is {}
+set :default_env, {
+  rbenv_root: "/usr/local/rbenv",
+  path: "/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH",
+  AWS_ACCESS_KEY_ID: ENV["AWS_ACCESS_KEY_ID"],
+  AWS_SECRET_ACCESS_KEY: ENV["AWS_SECRET_ACCESS_KEY"]
+}
 
   config.fog_directory  = 'freemarket-sample-51e'
   config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/freemarket-sample-51e'
