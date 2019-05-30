7ef6333141f56dedf3fdfd169dd12dde036e8ce1
hnmtk <50006387@users.noreply.github.com>
2019-05-30 17:52:00 +0900

    Delete space

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2019_05_30_064628) do
-  
+
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
     t.integer "parent_id"
