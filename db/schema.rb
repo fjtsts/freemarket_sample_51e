062bddc7b1ca89ba0c45b6800d1eed11dad0bdeb
hnmtk <50006387@users.noreply.github.com>
2019-05-31 17:37:25 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2019_05_30_101502) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -84,5 +83,4 @@ ActiveRecord::Schema.define(version: 2019_05_30_101502) do
 
   add_foreign_key "addresses", "users"
   add_foreign_key "user_profiles", "users"
-
 end
