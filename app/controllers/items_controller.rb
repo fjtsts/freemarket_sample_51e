c7892d37309816aca96d3f72d581ab4a05dd78bb
ShunsukeSugie <psku3ixdae@gmail.com>
2019-05-31 17:36:29 +0900

    commit

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
     def index
-        category1 = Category.find(1)
+        @category = Category.find(1)
+        @category1 = Category.find(2)
 
         @items = Item.all.order("created_at DESC").limit(4)
         
