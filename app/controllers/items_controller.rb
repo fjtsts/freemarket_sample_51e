79291ae27b87e963edf7f17dd1ae9ce259d7b15d
hnmtk <50006387@users.noreply.github.com>
2019-06-01 19:05:33 +0900

    Comment out error location

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,11 +2,11 @@ class ItemsController < ApplicationController
 # before_action :authenticate_user!, only: [:new]
 
     def index
-        category = Category.find(params[:id])
-        categories = [
-        category.children
-        category.children.map { |category| category.children }
-        ].flatten.compact
+        # category = Category.find(params[:id])
+        # categories = [
+        # category.children
+        # category.children.map { |category| category.children }
+        # ].flatten.compact
 
         @items = Item.all.order("created_at DESC").limit(4)
         
