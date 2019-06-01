483d193270203b76978e3a6eddf2ff2f10ee3da7
seiyaokado <seiya.okado0323@gmail.com>
2019-06-01 12:23:34 +0900

    view

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :items
-  resources :users, only: [:index, :show]
+  resources :users, only: [:index, :show, :edit]
   root      "items#index"
 end
