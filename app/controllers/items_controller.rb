6fc983a3272f620cd96a4935df1da96c6f718a6f
ShunsukeSugie <psku3ixdae@gmail.com>
2019-06-01 17:21:46 +0900

    commit

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,9 +1,13 @@
 class ItemsController < ApplicationController
     def index
-        @category = Category.find(1)
-        @category1 = Category.find(2)
-
-        @items = Item.all.order("created_at DESC").limit(4)
+        @category1 = Category.find(1)
+        @category2= Category.find(2)
+        @category3= Category.find(3)
+        @category4= Category.find(4)
+        @items =  @category1.items.all.order("created_at DESC").limit(4)
+        @items2 =  @category2.items.all.order("created_at DESC").limit(4)
+        @items3 =  @category3.items.all.order("created_at DESC").limit(4)
+        @items4 =  @category4.items.all.order("created_at DESC").limit(4)
         
     end
 
