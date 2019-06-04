1871ed4ee77d93d04fc078c2bb13311ff9b3582c
seiyaokado <seiya.okado0323@gmail.com>
2019-06-04 17:55:12 +0900

    a

--- app/assets/javascripts/item_view.js
+++ app/assets/javascripts/item_view.js
@@ -5,10 +5,3 @@ $(function(){
     $("#photoMain").attr('src',dataUrl);
   });
 });
-
-// $(function(){
-//   $(".thumbnail").on('mouseover',function(){
-//     var dataUrl = $(this).attr('data-url');
-//     $("#main-image").attr('src',dataUrl);
-//   });
-// });
