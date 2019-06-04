7c8a8feed0549cebcaf4eb18a833e6c70b9a0f3b
seiyaokado <seiya.okado0323@gmail.com>
2019-06-04 19:36:15 +0900

    a

--- app/assets/javascripts/item_view.js
+++ app/assets/javascripts/item_view.js
@@ -1,7 +1,19 @@
-
-$(function(){
-  $(".photosub").on('mouseover',function(){
-    var dataUrl = $(this).attr('data-url');
-    $("#photoMain").attr('src',dataUrl);
+$(function() {
+  $('.slider').slick({
+  accessibility: false,
+  adaptiveHeight: true,
+  arrows: false,
+  dots: true,
+  customPaging: function(slick, index){
+    var num = slick.$slides.eq(index).html();
+    return '<b>' + num + '</b>';
+  },
   });
 });
+
+// $(function(){
+//   $(".photosub").on('mouseover',function(){
+//     var dataUrl = $(this).attr('data-url');
+//     $("#photoMain").attr('src',dataUrl);
+//   });
+// });
