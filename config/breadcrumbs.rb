230236d23e62ce2a6b1d99181e56c96cb0f86dda
ShunsukeSugie <psku3ixdae@gmail.com>
2019-06-05 13:10:19 +0900

    commit

--- config/breadcrumbs.rb
+++ config/breadcrumbs.rb
@@ -15,6 +15,36 @@ crumb :show_user do |user|
   link user.name, user
   parent :users
 end
+crumb :card do
+  link "支払い方法", card_users_path
+  parent :mypage
+end
+
+
+crumb :profile do
+  link "プロフィール", edit_user_profile_path
+  parent :mypage
+end
+
+crumb :identification do
+  link "本人情報の登録", edit_address_path
+  parent :mypage
+end
+
+crumb :logout do
+  link "ログアウト", logout_users_path
+  parent :mypage
+end
+
+crumb :onsale do
+  link "出品した商品", onsale_user_items_path(current_user)
+  parent :mypage
+end
+
+crumb :phone do
+  link "電話番号の確認", phone_users_path
+  parent :mypage
+end
 
 # user#edit
 crumb :edit_user do |user|
