f40290a639251d3ff2afb060274a425fa9fb5b9b
hnmtk <50006387@users.noreply.github.com>
2019-06-05 15:12:57 +0900

    Association setting

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,9 +5,9 @@ class Item < ApplicationRecord
   has_many :item_images, dependent: :delete_all
   accepts_nested_attributes_for :item_images
   has_many :categories, through: :item_categories
-  has_many :item_categories, dependent: :delete_all
   belongs_to :category,optional: true
   belongs_to :brand,optional: true
+  has_one :exhibit, dependent: :destroy
   # belong_to  :size
 
   enum status: {"新品、未使用": 1, "未使用に近い": 2, "目立った傷や汚れなし": 3, "やや傷や汚れあり": 4, "傷や汚れあり": 5, "全体的に状態が悪い": 6}
