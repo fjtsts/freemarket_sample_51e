69b584966ee1fd42b15b2672309f8c5055b50d8d
ShunsukeSugie <psku3ixdae@gmail.com>
2019-06-06 14:23:40 +0900

    commit

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,3 +2,8 @@ class CategoriesController < ApplicationController
   def index
     @parents = Category.where(ancestry: nil)
   end
+  def show
+    @category = Category.find(params[:id])
+    @items = @category.items.order("created_at DESC").page(params[:page]).per(60)
+  end
+  end
