2dde58d40414e4f61163a9b83522fae69d265db2
hnmtk <50006387@users.noreply.github.com>
2019-06-07 18:24:21 +0900

    Display switching of size,brand

--- app/assets/javascripts/category.js
+++ app/assets/javascripts/category.js
@@ -1,6 +1,11 @@
 $(document).on('turbolinks:load',function(){
+
   $('#category_children').css("display" , "none");
   $('#category_grandchildren').css("display" , "none");
+  $('#form-wrap-size').css("display" , "none");
+  $('#form-wrap-brand').css("display" , "none");
+
+  // parent
   $("#category_parent").change(function(){
     var parent_id = $("#select_parent").val() - 1;
     var parent_category = "@parents["+parent_id+"]";
@@ -8,22 +13,32 @@ $(document).on('turbolinks:load',function(){
       $('#category_children').css("display" , "none");
       $('#category_grandchildren').css("display" , "none");
     } else {
-    console.log(parent_id);
       
       $('#category_children').css("display" , "");
     };
   });
 
+  // children
   $("#category_children").change(function(){
     var children_id = $("#select_children").val();
-    console.log(children_id);
     // var parent_category = "@parents["+parent_id+"]";
     if (children_id == "") {
       $('#category_grandchildren').css("display" , "none");
     } else {
-    console.log(children_id);
       
       $('#category_grandchildren').css("display" , "");
+    };
+  });
+
+  $("#category_grandchildren").change(function(){
+    var grandchildren_id = $("#select_grandchildren").val();
+    console.log(grandchildren_id);
+    if (grandchildren_id == "") {
+      $('#form-wrap-size').css("display" , "none");
+      $('#form-wrap-brand').css("display" , "none");
+    } else {
+      $('#form-wrap-size').css("display" , "");
+      $('#form-wrap-brand').css("display" , "");
     };
   });
 });
