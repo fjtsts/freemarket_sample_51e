23e4a6745ca29f1631fc1c562b8c0c72ef3473b6
shunsuke sugie <49972332+ShunsukeSugie@users.noreply.github.com>
2019-06-07 13:32:05 +0900

    Revert "[WIP]トップページビューアイテム一覧"

--- spec/factories/item.rb
+++ spec/factories/item.rb
@@ -11,5 +11,12 @@ FactoryBot.define do
   brand_id {"1"}
   sequence(:category_id) { |n| n }
   created_at { Faker::Time.between(2.days.ago, Time.now, :all) }
+  # after(:create) do |item|
+  #   create(item: item,category: create(:category),brand: create(:brand))
+  
+  # end
+
+ 
   end
+
 end
