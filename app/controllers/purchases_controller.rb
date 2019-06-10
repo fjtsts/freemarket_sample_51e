16805cd752a47cba97e8241a7d56567943a99eb1
ozkn <ozkn666@yahoo.co.jp>
2019-06-10 19:17:25 +0900

    show card linnk

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,7 +1,14 @@
 class PurchasesController < ApplicationController
+  before_action :authenticate_user!
 
   def index
     @item = Item.find(params[:item_id])
+    if current_user.card.present?
+      card = Card.where(user_id: current_user.id).first
+      Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
+      customer = Payjp::Customer.retrieve(card.customer_id)
+      @default_card_information = customer.cards.retrieve(card.card_id)
+    end
     render layout: 'form-layout'
   end
 
