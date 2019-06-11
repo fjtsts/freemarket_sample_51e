e7d4c65400d7b8acf74b8e1ae3a15dbd5e2e3a28
ozkn <ozkn666@yahoo.co.jp>
2019-06-11 13:33:06 +0900

    Update purchases_controller.rb

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -12,7 +12,17 @@ class PurchasesController < ApplicationController
     render layout: 'form-layout'
   end
 
-  def create
+  def pay
+    @item = Item.find(params[:item_id])
+    price = @item.price
+    card = Card.where(user_id: current_user.id).first
+    Payjp.api_key = ENV['PAYJP_PRIVATE_KEY']
+    Payjp::Charge.create(
+    :amount => price,
+    :customer => card.customer_id,
+    :currency => 'jpy',
+    )
+    @item.exhibit.status = "2" #change status 1 to 2
+    redirect_to controller: 'items', action: 'show', id: @item.id
   end
-
 end
