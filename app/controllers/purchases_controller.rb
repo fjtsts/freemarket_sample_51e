c1152dc0ba4023c6ff9959bb0134a63c3c3f3aa4
ozkn <ozkn666@yahoo.co.jp>
2019-06-11 14:57:40 +0900

    Update purchases_controller.rb

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -3,8 +3,8 @@ class PurchasesController < ApplicationController
   before_action :set_item, only: [:index, :pay]
 
   def index
-    if current_user.card
-      card = Card.where(user_id: current_user.id).first
+    if current_user.card.present?
+      card = Card.where(user_id: current_user.card).first
       Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
       customer = Payjp::Customer.retrieve(card.customer_id)
       @default_card_information = customer.cards.retrieve(card.card_id)
@@ -14,12 +14,12 @@ class PurchasesController < ApplicationController
 
   def pay
     price = @item.price
-    card = Card.where(user_id: current_user.id).first
+    card = Card.where(user_id: current_user.card).first
     Payjp.api_key = ENV['PAYJP_PRIVATE_KEY']
     Payjp::Charge.create(
-    :amount => price,
-    :customer => card.customer_id,
-    :currency => 'jpy',
+    amount: price,
+    customer: card.customer_id,
+    currency: 'jpy',
     )
     @item.exhibit.status = "2" #change status 1 to 2
     redirect_to controller: 'items', action: 'show', id: @item.id
