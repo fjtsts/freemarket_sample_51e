c5b30741d3c8ede65d1e8c22bd9992842c1d8bb4
ozkn <ozkn666@yahoo.co.jp>
2019-06-12 11:16:33 +0900

    create index sold view

--- app/controllers/exhibits_controller.rb
+++ app/controllers/exhibits_controller.rb
@@ -1,8 +1,10 @@
 class ExhibitsController < ApplicationController
   before_action :authenticate_user!
   def index
+    @exhibits = current_user.exhibits.where(status: 1).or(current_user.exhibits.where(status: 3))
   end
 
   def sold
+    @exhibits = current_user.exhibits.where(status: 2)
   end
 end
