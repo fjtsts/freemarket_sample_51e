fedcd0e2b8a5ceeae02fda6be140290471554b20
ozkn <ozkn666@yahoo.co.jp>
2019-06-12 13:59:24 +0900

    create review system

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,11 +2,20 @@ class ReviewsController < ApplicationController
   before_action :authenticate_user!
 
   def create
+    @review
+    @review = Review.create(review_params)
+    redirect_to controller: 'items', action: 'show', id: params[:item_id]
   end
 
   def new
     @item = Item.find(params[:item_id])
     @review = Review.new
     render layout: 'form-layout'
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:status, :content).merge(user_id: current_user.id, item_id: params[:item_id])
   end
 end
