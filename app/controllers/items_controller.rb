7553dd3d9721a8d59faf7a19d08000a5e854538c
ShunsukeSugie <psku3ixdae@gmail.com>
2019-06-13 21:30:15 +0900

    commit

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,6 +3,7 @@ before_action :authenticate_user!, only: [:new, :show, :create]
 before_action :set_parents, only: [:new, :edit]
 
     def index
+        $query = Item.ransack(params[:q])
         @query= Item.ransack(params[:q])
         @ladies =Item.where(category_id: Category.first.subtree_ids).all.order(created_at: "DESC").limit(4)
         @mens =  Item.where(category_id: Category.second.subtree_ids).all.order(created_at: "DESC").limit(4)
@@ -89,8 +90,8 @@ before_action :set_parents, only: [:new, :edit]
 
     def search
         @parents = Category.where(ancestry: nil)
-        @query = Item.ransack(params[:q])
-        @items = @query.result.includes(:category, :brand)
+        $query = Item.ransack(params[:q])
+        @items = $query.result.includes(:category, :brand)
     end
 
     private
