b860609e32fd36f1ee4a9d298a2479f1df0afd74
satoshi fujita <xxxx3104@yahoo.co.jp>
2019-06-18 14:01:08 +0900

    Update like-function

--- app/controllers/favorite_items_controller.rb
+++ app/controllers/favorite_items_controller.rb
@@ -1,16 +1,20 @@
 class FavoriteItemsController < ApplicationController
 
   before_action :set_variables
-  
-  def create
-    @favorite_item = FavoriteItem.create(user_id: current_user.id, item_id: params[:item_id])
-    @favorite_items = FavoriteItem.where(item_id: params[:item_id])
-  end
 
-  def destroy
-    @favorite_item = FavoriteItem.find_by(user_id: current_user.id, item_id: params[:item_id])
-    @favorite_item.destroy
-    @favorite_items = FavoriteItem.where(item_id: params[:item_id])
+  begin
+    def create
+      @favorite_item = FavoriteItem.create(user_id: current_user.id, item_id: params[:item_id])
+      @favorite_items = FavoriteItem.where(item_id: params[:item_id])
+    end
+
+    def destroy
+      @favorite_item = FavoriteItem.find_by(user_id: current_user.id, item_id: params[:item_id])
+      @favorite_item.destroy
+      @favorite_items = FavoriteItem.where(item_id: params[:item_id])
+    end
+  rescue => e
+    puts "error"
   end
 
   private
