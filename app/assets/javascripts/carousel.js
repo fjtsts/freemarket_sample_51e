e0555672adca9217e90493ae6cbf9e8ecb341096
ozkn <ozkn666@yahoo.co.jp>
2019-06-19 13:24:19 +0900

    comment out Swiper

--- app/assets/javascripts/carousel.js
+++ app/assets/javascripts/carousel.js
@@ -1,24 +1,25 @@
 
-$(function (){
-  $(document).on('turbolinks:load', function(){
-  var mySwiper = new Swiper('.swiper-container',{
-    mode: 'horizontal',
-    autoHeight: true,
-    loop: true,
-    autoplay: {
-      delay: 3000,
-      disableOnInteraction: false,
-      reverseDirection: false
-    },
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev'
-    },
-    pagination: {
-      el: '.swiper-pagination',
-      type: 'bullets',
-      clickable: true
-    }
-  });
-  })
-});
+// エラーが解消されるまで、暫定的にコメントアウトしています。
+// $(function (){
+//   $(document).on('turbolinks:load', function(){
+//   var mySwiper = new Swiper('.swiper-container',{
+//     mode: 'horizontal',
+//     autoHeight: true,
+//     loop: true,
+//     autoplay: {
+//       delay: 3000,
+//       disableOnInteraction: false,
+//       reverseDirection: false
+//     },
+//     navigation: {
+//       nextEl: '.swiper-button-next',
+//       prevEl: '.swiper-button-prev'
+//     },
+//     pagination: {
+//       el: '.swiper-pagination',
+//       type: 'bullets',
+//       clickable: true
+//     }
+//   });
+//   })
+// });
