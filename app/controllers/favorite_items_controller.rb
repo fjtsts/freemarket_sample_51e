2031e3b5ffe64ca7b7cde49f0e35a6d5a22892f1
satoshi fujita <xxxx3104@yahoo.co.jp>
2019-06-19 15:45:49 +0900

    Update favorite_items_controller.rb

--- app/controllers/favorite_items_controller.rb
+++ app/controllers/favorite_items_controller.rb
@@ -2,18 +2,22 @@ class FavoriteItemsController < ApplicationController
 
   before_action :set_item
 
-  # begin
     def create
       @favorite_item = FavoriteItem.create(user_id: current_user.id, item_id: params[:item_id])
+      if @favorite_item.save
+      else
+      end
       @item.reload
     end
 
     def destroy
-      favorite_item = FavoriteItem.find_by(user_id: current_user.id, item_id: params[:item_id])
-      favorite_item.destroy
+      @favorite_item = FavoriteItem.find_by(user_id: current_user.id, item_id: params[:item_id])
+      @favorite_item.destroy
+      if @favorite_item.save
+      else
+      end
       @item.reload
     end
-  # end
 
   private
 
