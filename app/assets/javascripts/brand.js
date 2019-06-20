f37120777d54da0f629fdefcf66d5027486e3498
ShunsukeSugie <psku3ixdae@gmail.com>
2019-06-20 14:53:16 +0900

    commit

--- app/assets/javascripts/brand.js
+++ app/assets/javascripts/brand.js
@@ -2,7 +2,8 @@ $(function (){
     $('.category-container__list-box').hide().eq(0).show();
     $('.parent-link').click(function(){
       var index = $('.parent-link').index(this);
-      $('.category-container__list-box').eq(index).show();
       debugger
+      $('.category-container__list-box').eq(index).show();
+      
     });
   });
