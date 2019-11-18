a3630d7446079dd50ae48935fd83ed39c973c5ee
ShunsukeSugie <psku3ixdae@gmail.com>
2019-11-18 15:01:53 +0900

    commit

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,7 @@ module FreemarketSample51e
     config.active_record.default_timezone = :local
     config.i18n.default_locale = :ja
 
-
+    config.logger = Logger.new(STDOUT)
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
